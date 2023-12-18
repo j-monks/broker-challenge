ddf795439d488cf09050af0e6c14e7d0783d0ebd
j-monks <j.monks23@gmail.com>
2023-12-18 15:07:13 +0000

    Added a landing page with a brief description.
    Configured routing to include the landing page.

---
+++ packages/frontend/src/views/LandingPageView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="landing-page">
+    <div class="centered-content">
+      <h1>Welcome to my Broker Insights Challenge Submission</h1>
+      <p>To get started, upload your insurance policy data via CSV by clicking the "Upload Policies" button in the header.</p>
+      <p>Alternatively, if you have already uploaded a CSV file, you can view your policy data by clicking the "View Policies" button.</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LandingPageView',
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+
+<style>
+.landing-page {
+  display: flex;
+  justify-content: center;
+  height: 100vh;
+  padding-top: 50px;
+}
+
+.centered-content {
+  text-align: center;
+}
+</style>
