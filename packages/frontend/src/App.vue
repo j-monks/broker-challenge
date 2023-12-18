8107cbf7d28fc388d6b93dc3637135cf1fc7ab0c
j-monks <j.monks23@gmail.com>
2023-12-18 14:35:15 +0000

    Modified App.vue to include routes for uploading policies and viewing them
    Added styling inspired by broker insights

--- packages/frontend/src/App.vue
+++ packages/frontend/src/App.vue
@@ -1,44 +1,38 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
+    <!-- Header -->
+    <v-app-bar app color="#F19721">
+      <router-link to="/">
         <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
+          class="mr-2"
+          src="https://cdn-knepb.nitrocdn.com/KVGQfzJAbiZbbyecyJahDkfKRfRdusoe/assets/images/optimized/rev-c366493/brokerinsights.com/wp-content/themes/brokersv2/assets/img/Broker-Insights-logo.gif"
+          max-height="150"
+          max-width="150"
           contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+        ></v-img>
+      </router-link>
 
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <router-link to="/upload-policies">
+        <v-btn plain>
+          <v-icon>mdi-cloud-upload</v-icon>
+          Upload Policies
+        </v-btn>
+      </router-link>
+      <router-link to="/view-policies">
+        <v-btn plain>
+          <v-icon>mdi-file-document</v-icon>
+          View Policies
+        </v-btn>
+      </router-link>
     </v-app-bar>
 
+    <!-- Content Area -->
     <v-main>
-      <router-view/>
+      <v-container fluid>
+        <router-view/>
+      </v-container>
     </v-main>
   </v-app>
 </template>
